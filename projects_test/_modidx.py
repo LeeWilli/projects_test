be9ca28cf416133669eaf63f964ad68d2dbfcfc5
汪力 <wangli@xinktech.com>
2023-07-27 13:12:09 +0800

    add test post_video

--- projects_test/_modidx.py
+++ projects_test/_modidx.py
@@ -5,5 +5,5 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://LeeWilli.github.io',
                 'git_url': 'https://github.com/LeeWilli/projects_test',
                 'lib_path': 'projects_test'},
-  'syms': { 'projects_test.core': { 'projects_test.core.detect_pulse': ('heart.html#detect_pulse', 'projects_test/core.py'),
-                                    'projects_test.core.send_frame': ('heart.html#send_frame', 'projects_test/core.py')}}}
+  'syms': { 'projects_test.core': { 'projects_test.core.detect_pulse': ('heart-post-image.html#detect_pulse', 'projects_test/core.py'),
+                                    'projects_test.core.send_frame': ('heart-post-image.html#send_frame', 'projects_test/core.py')}}}
